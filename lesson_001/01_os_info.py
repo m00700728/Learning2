3edaa85eaac99dfb9280f07c085d120407d7e62b
m00700728 <83155713+m00700728@users.noreply.github.com>
2021-04-25 11:29:37 +0300

    environment versions

---
+++ lesson_001/01_os_info.py
@@ -0,0 +1,10 @@
+# TODO запустить этот код и сделать коммит на git
+
+import sys
+import platform
+
+info = 'OS info is \n{} \n\nPython version is {}'.format(
+    platform.uname(), sys.version, platform.architecture())
+print(info)
+with open('os_info.txt', 'w') as ff:
+    ff.write(info)
